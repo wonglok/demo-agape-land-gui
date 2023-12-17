bcf85b4a8b4f5c53013193c21df213af2155e620
Wong Lok <wonglok@users.noreply.github.com>
2023-12-17 21:43:33 +0800

    face

--- src/components/Sound/CleanGPU/simulation/velSimShader.js
+++ src/components/Sound/CleanGPU/simulation/velSimShader.js
@@ -39,217 +39,216 @@ float opXor(float d1, float d2 )
 }
 
 float constrain(float val, float min, float max) {
-        if (val < min) {
-            return min;
-        } else if (val > max) {
-            return max;
-        } else {
-            return val;
-        }
-      }
+  if (val < min) {
+      return min;
+  } else if (val > max) {
+      return max;
+  } else {
+      return val;
+  }
+}
+
+vec3 getDiff (in vec3 lastPos, in vec3 mousePos) {
+  vec3 diff = lastPos - mousePos;
+
+  float distance = constrain(length(diff), 30.0, 150.0);
+
+  // v is extra speed
+  float strength = 3.0 * 1.0 / pow(distance, 2.0);
+
+  diff = normalize(diff);
+  diff = diff * pow(strength, 1.0) * -2.0;
+
+  // extra strength
+  diff *= 10.0;
+
+  return diff;
+}
 
 
-      vec3 getDiff (in vec3 lastPos, in vec3 mousePos) {
-        vec3 diff = lastPos - mousePos;
+vec3 multiverse (vec3 pp) {
+  vec3 p = vec3(pp);
+  vec3 v = vec3(0.0);
 
-        float distance = constrain(length(diff), 30.0, 150.0);
+  // many lines of multiverse
+  p.xy *= 10.0;
+  p.x += 11.0;
+  v.x = cos((max(p.x, p.y) - min(length(p), p.y)));
+  v.y = cos(max((p.y - cos(length(p))),p.x)) * cos((p.x - p.x));
+  v.xy *= 1.0;
 
-        // v is extra speed
-        float strength = 3.0 * 1.0 / pow(distance, 2.0);
+  return v;
+}
 
-        diff = normalize(diff);
-        diff = diff * pow(strength, 1.0) * -2.0;
+vec3 galaxy (vec3 pp) {
+  vec3 p = vec3(pp);
+  vec3 v = vec3(0.0);
 
-        // extra strength
-        diff *= 10.0;
+  // galaxy
+  v.x += p.y;
+  v.y += sin(sin((min(exp(length(p)),p.y)-p.x)));
 
-        return diff;
-      }
+  return v;
+}
 
+vec3 circle (vec3 pp) {
+  vec3 p = vec3(pp);
+  vec3 v = vec3(0.0);
 
-      vec3 multiverse (vec3 pp) {
-        vec3 p = vec3(pp);
-        vec3 v = vec3(0.0);
+  // circle
+  v.x += -p.y;
+  v.z += p.z;
 
-        // many lines of multiverse
-        p.xy *= 10.0;
-        p.x += 11.0;
-        v.x = cos((max(p.x, p.y) - min(length(p), p.y)));
-        v.y = cos(max((p.y - cos(length(p))),p.x)) * cos((p.x - p.x));
-        v.xy *= 1.0;
-
-        return v;
-      }
-
-      vec3 galaxy (vec3 pp) {
-        vec3 p = vec3(pp);
-        vec3 v = vec3(0.0);
-
-        // galaxy
-        v.x += p.y;
-        v.y += sin(sin((min(exp(length(p)),p.y)-p.x)));
-
-        return v;
-      }
-
-      vec3 circle (vec3 pp) {
-        vec3 p = vec3(pp);
-        vec3 v = vec3(0.0);
-
-        // circle
-        v.x += -p.y;
-        v.z += p.z;
-
-        return v;
-      }
+  return v;
+}
 
 
-      vec3 river (vec3 pp) {
-        vec3 p = vec3(pp);
-        vec3 v = vec3(0.0);
+vec3 river (vec3 pp) {
+  vec3 p = vec3(pp);
+  vec3 v = vec3(0.0);
 
-        // river
-        v.x += sin(length(p));
-        v.y += cos((length(p)-p.x*p.y));
+  // river
+  v.x += sin(length(p));
+  v.y += cos((length(p)-p.x*p.y));
 
-        return v;
-      }
-
-
-      vec3 squares (vec3 pp) {
-        vec3 p = vec3(pp * 2.7);
-        vec3 v = vec3(0.0);
-
-        v.x = p.y;
-        v.y = p.x/length(p)/max(p.y,sin(length(p)));
-
-        return v;
-      }
+  return v;
+}
 
 
-      vec3 funSwirl (vec3 pp) {
-        vec3 p = vec3(pp);
-        vec3 v = vec3(0.0);
+vec3 squares (vec3 pp) {
+  vec3 p = vec3(pp * 2.7);
+  vec3 v = vec3(0.0);
 
-        p *= 3.5;
-        p.y += 1.5;
-        p.x *= 2.5;
-        v.x += cos(p.x + 2.0 * p.y);
-        v.y += sin(p.x - 2.0 * p.y);
-        v.xy *= 0.5;
+  v.x = p.y;
+  v.y = p.x/length(p)/max(p.y,sin(length(p)));
 
-        return v;
-      }
+  return v;
+}
 
 
-      vec3 stoneFlow (vec3 pp) {
-        vec3 p = vec3(pp);
-        vec3 v = vec3(0.0);
+vec3 funSwirl (vec3 pp) {
+  vec3 p = vec3(pp);
+  vec3 v = vec3(0.0);
 
-        // flow to a stone
-        p *= 2.0 * 2.0 * 3.1415;
-        v.x = p.y - length(p);
-        v.y = length(p);
-        v.xy *= 1.0;
+  p *= 3.5;
+  p.y += 1.5;
+  p.x *= 2.5;
+  v.x += cos(p.x + 2.0 * p.y);
+  v.y += sin(p.x - 2.0 * p.y);
+  v.xy *= 0.5;
 
-        return v;
-      }
-
-      vec3 jade (vec3 pp) {
-        vec3 p = vec3(pp);
-        vec3 v = vec3(0.0);
-
-        p.xy *= 10.0;
-        v.x = sin(min(p.y,length(p)));
-        v.y = (sin(p.y)-p.x);
-        v.xy *= 1.0;
+  return v;
+}
 
 
-        return v;
-      }
+vec3 stoneFlow (vec3 pp) {
+  vec3 p = vec3(pp);
+  vec3 v = vec3(0.0);
 
-      vec3 converge (vec3 pp) {
-        vec3 p = vec3(pp);
-        vec3 v = vec3(0.0);
+  // flow to a stone
+  p *= 2.0 * 2.0 * 3.1415;
+  v.x = p.y - length(p);
+  v.y = length(p);
+  v.xy *= 1.0;
 
-        p.xy *= 2.0;
-        p.y += 1.0;
-        v.x = length(p);
-        v.y = cos(p.y);
-        v.xy *= 1.0;
+  return v;
+}
 
-        return v;
-      }
+vec3 jade (vec3 pp) {
+  vec3 p = vec3(pp);
+  vec3 v = vec3(0.0);
 
-      vec3 boxedSwirl (vec3 pp) {
-        vec3 p = vec3(pp);
-        vec3 v = vec3(0.0);
+  p.xy *= 10.0;
+  v.x = sin(min(p.y,length(p)));
+  v.y = (sin(p.y)-p.x);
+  v.xy *= 1.0;
 
-        p.xyz *= 3.1415 * 2.0 * 1.5;
-        v.x = sin(sin(time) + p.y);
-        v.y = cos(sin(time) + p.x);
-        v.z = cos(sin(time) + p.z);
 
-        v.xyz *= 1.0;
+  return v;
+}
 
-        return v;
-      }
+vec3 converge (vec3 pp) {
+  vec3 p = vec3(pp);
+  vec3 v = vec3(0.0);
 
-      vec3 passWave (vec3 pp) {
-        vec3 p = vec3(pp);
-        vec3 v = vec3(0.0);
+  p.xy *= 2.0;
+  p.y += 1.0;
+  v.x = length(p);
+  v.y = cos(p.y);
+  v.xy *= 1.0;
 
-        p.xy *= 2.0 * 3.1415;
-        p.x += 4.0;
-        v.x = max(sin(p.x),p.x)/p.x;
-        v.y = (min(exp(cos(length(p))),sin(p.x))+cos(p.x));
+  return v;
+}
 
-        return v;
-      }
+vec3 boxedSwirl (vec3 pp) {
+  vec3 p = vec3(pp);
+  vec3 v = vec3(0.0);
 
-      vec3 get_vel (vec3 pp) {
-        float timer = mod(time * 0.05, 1.0);
-        if (timer < 0.5) {
-          return funSwirl(pp) * 0.5;
-        } else {
-          return funSwirl(pp) * -0.5;
-        }
-      }
+  p.xyz *= 3.1415 * 2.0 * 1.5;
+  v.x = sin(sin(time) + p.y);
+  v.y = cos(sin(time) + p.x);
+  v.z = cos(sin(time) + p.z);
 
-      #define M_PI_3_1415 3.1415926535897932384626433832795
+  v.xyz *= 1.0;
 
-      float atan2(in float y, in float x) {
-        bool xgty = (abs(x) > abs(y));
-        return mix(M_PI_3_1415 / 2.0 - atan(x,y), atan(y,x), float(xgty));
-      }
+  return v;
+}
 
-      vec3 fromBall(float r, float az, float el) {
-        return vec3(
-          r * cos(el) * cos(az),
-          r * cos(el) * sin(az),
-          r * sin(el)
-        );
-      }
-      void toBall(vec3 pos, out float az, out float el) {
-        az = atan2(pos.y, pos.x);
-        el = atan2(pos.z, sqrt(pos.x * pos.x + pos.y * pos.y));
-      }
+vec3 passWave (vec3 pp) {
+  vec3 p = vec3(pp);
+  vec3 v = vec3(0.0);
 
-      // float az = 0.0;
-      // float el = 0.0;
-      // vec3 noiser = vec3(lastVel);
-      // toBall(noiser, az, el);
-      // lastVel.xyz = fromBall(1.0, az, el);
+  p.xy *= 2.0 * 3.1415;
+  p.x += 4.0;
+  v.x = max(sin(p.x),p.x)/p.x;
+  v.y = (min(exp(cos(length(p))),sin(p.x))+cos(p.x));
 
-      vec3 ballify (vec3 pos, float r) {
-        float az = atan2(pos.y, pos.x);
-        float el = atan2(pos.z, sqrt(pos.x * pos.x + pos.y * pos.y));
-        return vec3(
-          r * cos(el) * cos(az),
-          r * cos(el) * sin(az),
-          r * sin(el)
-        );
-      }
+  return v;
+}
+
+vec3 get_vel (vec3 pp) {
+  float timer = mod(time * 0.05, 1.0);
+  if (timer < 0.5) {
+    return funSwirl(pp) * 0.5;
+  } else {
+    return funSwirl(pp) * -0.5;
+  }
+}
+
+#define M_PI_3_1415 3.1415926535897932384626433832795
+
+float atan2(in float y, in float x) {
+  bool xgty = (abs(x) > abs(y));
+  return mix(M_PI_3_1415 / 2.0 - atan(x,y), atan(y,x), float(xgty));
+}
+
+vec3 fromBall(float r, float az, float el) {
+  return vec3(
+    r * cos(el) * cos(az),
+    r * cos(el) * sin(az),
+    r * sin(el)
+  );
+}
+void toBall(vec3 pos, out float az, out float el) {
+  az = atan2(pos.y, pos.x);
+  el = atan2(pos.z, sqrt(pos.x * pos.x + pos.y * pos.y));
+}
+
+// float az = 0.0;
+// float el = 0.0;
+// vec3 noiser = vec3(lastVel);
+// toBall(noiser, az, el);
+// lastVel.xyz = fromBall(1.0, az, el);
+
+vec3 ballify (vec3 pos, float r) {
+  float az = atan2(pos.y, pos.x);
+  float el = atan2(pos.z, sqrt(pos.x * pos.x + pos.y * pos.y));
+  return vec3(
+    r * cos(el) * cos(az),
+    r * cos(el) * sin(az),
+    r * sin(el)
+  );
+}
 
 
 
@@ -428,6 +427,7 @@ void main (void) {
   vel.rgb += getDiff(pos.rgb, mouseNow) * dt * 1.0 * 1.0;
   collisionMouseSphere(1.0, pos, vel.rgb, 5.0);
 
+  vel.rgb += galaxy(pos.xyz * 0.01);
 
   gl_FragColor = vel;
 }
